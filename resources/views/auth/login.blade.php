e5c5d3d417764cde66d5d2a1112be42e5b0e4ad8
ZofP <petr.zofak@gmail.com>
2021-11-15 17:29:58 +0100

    modified the login page, added buttons

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,9 +2,14 @@
 
 @section('content')
 
-    <form action="{{ route('login') }}" method="post">
+<div>
+    <h2>Without logging in you can't create group searches or save your results!</h2>
+    <button><a href="/anonymous-login">Continue without Login</a></button>
+</div>
+
+<form action="{{ route('login') }}" method="post">
     @csrf
- 
+
     <label for="email">Email: </label>
     <input type="email" name="email" id="email" value="{{ old('email') }}">
     <br>
@@ -12,7 +17,9 @@
     <input type="password" name="password" id="pass" value="">
     <br>
     <button>Login</button>
- 
+
 </form>
+
+<div><button><a href="/register">Create an account</a></button></div>
 
 @endsection
