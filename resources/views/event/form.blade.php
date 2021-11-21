d344bbc5bbd0b503db6b4233d5a8420d57c8c4f1
Jachym <jachympivonka@gmail.com>
2021-11-21 20:44:35 +0100

    Edit function done

--- resources/views/event/form.blade.php
+++ resources/views/event/form.blade.php
@@ -1,8 +1,10 @@
 @extends('layout/main')
 
 @section('content')
+
+
     
-    <div id="app">
+    <div id="app" @if(isset($id)) data-id="{{ $id }}" @endif>
         <script src={{mix('js/event-create.js')}}></script>
     </div>
     
