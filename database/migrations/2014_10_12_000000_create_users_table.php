1b563cc1f0cf22f7fd86232676adbf930a6c194f
ZofP <petr.zofak@gmail.com>
2021-11-22 17:18:10 +0100

    changed user migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,6 +22,7 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('phone')->nullable();
             $table->string('gender')->nullable();
+            $table->unsignedBigInteger('default_group_id')->unique();
             $table->rememberToken();
             $table->timestamps();
         });
