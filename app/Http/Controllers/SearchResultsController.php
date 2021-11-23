9b5f8a68834533a5ec4d8287f60ebf61273a6083
cyclopicdog <jonathan_doig@hotmail.com>
2021-11-23 18:49:49 +0100

    making arrays in the searchresultscontroller

--- app/Http/Controllers/SearchResultsController.php
+++ app/Http/Controllers/SearchResultsController.php
@@ -24,11 +24,22 @@ class SearchResultsController extends Controller
             $user_sessions[] = SearchSession::find($choice->session_id);
         }
 
-
+        $user_choices = [];
+foreach($user_sessions as $session)
+{
+    $user_choices[] = $session->user_choices;
+}
+// $users = [];
+// foreach($user_choices as $set)
+// {
+// $users[] = User::find($set->user_id);
+// }
         // $user_session_info = User::with(['user_choices', 'user_choices.search_sessions', 'user_choices.search_sessions.group'])
         //     ->where('id', $user_id)
         //     ->get();
         // $user_sessions = $user_session_info[0];
+        return $user_choices[0][0]->user;
+        
         return $user_sessions;
         return $user_info->user_choices;
         return view('search.sessions', compact('user_sessions'));
