fa90ce67a4466b2e2f8eaaea261aabe9973e3ca6
cyclopicdog <jonathan_doig@hotmail.com>
2021-11-25 20:09:15 +0100

    results page styling

--- resources/js/SearchPage/components/SearchResults/SearchResults.js
+++ resources/js/SearchPage/components/SearchResults/SearchResults.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import './SearchResults.scss'
+import './SearchResults.scss';
 
 const SearchResults = ({
     // results: { event, group_choices }
@@ -17,39 +17,57 @@ const SearchResults = ({
     //     return null
     // }
 
-
     return (
-        <div className='results-container'>
-            {event ?
-                <>
-                    <h3>{message}</h3>
-                    <h2> {event.name}</h2>
-                    <h4>Event match score: {score}%</h4>
-                    <p>Venue: {event.venue.name}</p>
-                    <p>{event.description}</p>
-                    <p>Date: {event.start_date}</p>
-                    <p>Starts at: {event.start_time}</p>
-                    <p>Ends at: {event.end_time}</p>
-                    <p>Address: {event.venue.address}</p>
-                    <a href={event.venue.website}>{event.venue.name}</a>
+        <div className="results">
+            {event ? (
+                <div className="results__container">
+                    <h3 className="results__message">{message}</h3>
+                    <h2 className="results__title"> {event.name}</h2>
+                    <h4 className="results__score">
+                        Event match score: {score}%
+                    </h4>
+                    <h4 className="results__score">
+                        <span className="results__category">Venue:</span>{' '}
+                        {event.venue.name}
+                    </h4>
+                    <p className="results__info">{event.description}</p>
+                    <p className="results__info">
+                        <span className="results__category">Date:</span>{' '}
+                        {event.start_date}
+                    </p>
+                    <p className="results__info">
+                        <span className="results__category">Starts at:</span>{' '}
+                        {event.start_time}
+                    </p>
+                    <p className="results__info">
+                        <span className="results__category">Ends at:</span>{' '}
+                        {event.end_time}
+                    </p>
+                    <p className="results__info">
+                        <span className="results__category">Address:</span>{' '}
+                        {event.venue.address}
+                    </p>
+                    <a className="results__link" href={event.venue.website}>
+                        {event.venue.name}
+                    </a>
                     {event.venue.map_link.includes('embed') && (
                         <iframe
+                            className="results__map"
                             src={event.venue.map_link}
-                            width="600"
-                            height="450"
+                            width="500"
+                            height="200"
                             // style="border:0;"
                             allowFullScreen
 
-                        // loading="lazy"
+                            // loading="lazy"
                         ></iframe>
                     )}
-                </>
-                :
+                </div>
+            ) : (
                 <h3>{message}</h3>
-
-            }
+            )}
         </div>
-    )
-}
+    );
+};
 
 export default SearchResults;
